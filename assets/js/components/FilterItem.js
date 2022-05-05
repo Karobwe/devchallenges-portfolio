067f33c4866773a976a9ddffcd59ed46c3e7ee1f
Chanfi Attoumanni <attoumani.chanfi@gmail.com>
2022-05-05 13:44:50 +0200

    feat: refactor filter item component

--- assets/js/components/FilterItem.js
+++ assets/js/components/FilterItem.js
@@ -8,33 +8,19 @@ export class FilterItem extends React.Component {
         super(props);
 
         this.state = {
-            itemClass: "filter__item"
+            isActive: false,
         }
     }
 
-    filterItemClickHandler = (e) => {
-        let items = this.state.itemClass.split(" ");
-
-        if(items.indexOf("active") === -1) {
-            items.push("active");
-
-            this.setState((prevState) => {
-                return {itemClass: (prevState.itemClass + " active").trim()}
-            });
-        } else {
-            items = items.splice(items.indexOf("active"), 1);
-
-            this.setState((prevState) => {
-                return {itemClass: prevState.itemClass.split(" ").splice(items.indexOf("active"), 1).join(" ")}
-            });
-        }
-
-        // All above code job could be done without state and in one line: e.target.classList.toggle("active");
+    filterItemClickHandler = () => {
+        this.setState((state) => {
+            return {isActive: !state.isActive};
+        });
     }
 
     render() {
         return (
-            <div className={this.state.itemClass} onClick={(e) => this.filterItemClickHandler(e)}>
+            <div className={`filter__item ${this.state.isActive ? 'active' : ''}`.trim()} onClick={() => this.filterItemClickHandler()}>
                 {this.props.text}
             </div>
         );
